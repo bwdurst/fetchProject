0c419835121d143e793a6aa07cdfcade050b4986
bwdurst <bwdurst@gmail.com>
2019-12-15 20:00:28 -0600

    finalized fetch project

--- main.js
+++ main.js
@@ -1,51 +1,53 @@
-//initial commentlet arrayOfPosts;
+// binding to hold requested posts
 let arrayOfPosts;
 
-// this function waits for the web page to be loaded, when it does it will run the code inside of it which happen to be getPosts()
+// loads posts into the arrayOfPosts array upon window load
 window.onload = function() {
   getPosts()
 
 }
 
-// this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
+// binding to request posts through the API
 const getPosts = () => {
   fetch('https://randomuser.me/api/?results=100')
     .then(res => res.json())
     .then(posts => arrayOfPosts = posts)
 }
 
-// this function logs the results in your browsers console
+// logs the arrayOfPosts array in the browsers console
 const consolePosts = () => {
   console.log(arrayOfPosts)
 }
 
-// this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
+// this function creates DOM elements and adds the correct properties/text so they can be viewed.
 const displayPost = () => {
   const allPosts = document.getElementById('all-posts')
   arrayOfPosts.results.map((post) => {
     const li = document.createElement('li')
     const photo = document.createElement('img')
+    const postId = post.login.username;
     const text = document.createTextNode(`${post.name.first} ${post.name.last}`)
+
+    // these bindings will be called when the See Details button is clicked, displaying more granular details for each post
     const details = document.createElement('p')
     const detailsButton = document.createElement('button')
-    const postId = post.login.username;
-
     detailsButton.setAttribute('onclick', "addDetails(" + postId +")")
     detailsButton.innerHTML = 'See details'
-    details.innerHTML = `Email: ${post.email} DOB: ${post.dob.date}`
-    photo.setAttribute('src', post.picture.medium)
+    details.innerHTML = `Email: ${post.login.username} <br> ${post.email} <br> Username DOB: ${post.dob.date}`
     details.setAttribute('id', postId)
-    // li.setAttribute('id', postId)
+
+    // these lines will add all of the elements to the DOM. Details will be hidden initially
+    photo.setAttribute('src', post.picture.medium)
     li.appendChild(photo)
     li.appendChild(text)
     li.appendChild(details)
     li.appendChild(detailsButton)
     allPosts.append(li)
     document.getElementById(postId).style.display = 'none';
-
   })
 }
 
+// this function will display the details that were hidden when the post element was created
 const addDetails = (postId) => {
   document.getElementById(postId.id).style.display = "block"
 }
